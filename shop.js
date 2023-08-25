93d18f548a8926e7a3025f9f1de34d3bcab1c286
Bruno Eduardo <94810700+BrunoEduardo45@users.noreply.github.com>
2023-08-25 11:23:29 -0300

    Ajustes na listagem dos produtos

--- shop.js
+++ shop.js
@@ -1,7 +1,10 @@
+// Tipo 1 = Masculono
+// Tipo 2 = Feminino
+// Tipo 3 = Acessórios
 const items = [
     {
         id: 0,
-        tipo: 0,
+        tipo: 1,
         nome: 'Calça Masculina',
         descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         img: 'img/calca-masculina.webp',
@@ -12,7 +15,7 @@ const items = [
     },
     {
         id: 1,
-        tipo: 0,
+        tipo: 1,
         nome: 'Camisa Masculina',
         descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         img: '/img/camisa-masculina.webp',
@@ -23,7 +26,7 @@ const items = [
     },
     {
         id: 2,
-        tipo: 1,
+        tipo: 2,
         nome: 'Short Feminino',
         descricao: 'Lorem Ips typesetting industry.',
         img: '/img/short-feminino.webp',
@@ -34,7 +37,7 @@ const items = [
     },
     {
         id: 3,
-        tipo: 1,
+        tipo: 2,
         nome: 'Calça Feminina',
         descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         img: '/img/calca-feminina.webp',
@@ -45,7 +48,7 @@ const items = [
     },
     {
         id: 4,
-        tipo: 1,
+        tipo: 2,
         nome: 'Camisa Feminina',
         descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         img: '/img/camisa-feminina.jfif',
@@ -56,7 +59,7 @@ const items = [
     },
     {
         id: 5,
-        tipo: 2,
+        tipo: 3,
         nome: 'Mochila de criança',
         descricao: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
         img: '/img/mochila-crianca.jfif',
@@ -67,49 +70,7 @@ const items = [
     },
 ]
 
-inicializarLoja = () => {
-    var containerProdutos = document.getElementById('produtos');
-    items.map((val)=>{
-        containerProdutos.innerHTML += `
-
-        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 articles">
-            <div class="articles-content">
-                <img src="`+ val.img +`" alt="" srcset="">
-                <br/>
-                <h3>`+val.nome+`</h3>
-                <p>`+val.descricao+`</p>
-
-                <div class="div-size mb-3">
-                    <button class="size">PP</button>
-                    <button class="size">P</button>
-                    <button class="size">M</button>
-                    <button class="size">G</button>
-                    <button class="size">GG</button>
-                </div>
-
-                <select class="form-select mb-3" aria-label="Default select example">
-                    <option selected>Selecione</option>
-                    <option value="1">Preto</option>
-                    <option value="2">Azul</option>
-                    <option value="3">Vermelho</option>
-                    <option value="4">Rosa</option>
-                </select>
-
-                <p class="w-100 text-center p-2 rounded" 
-                style="font-size:18px; background-color: #eeeeee; font-weight: 600;"> 
-                    R$ `+val.valor+`
-                </p>
-
-                <a key="`+val.id+`" class="btn btn-outline-success d-flex justify-content-center align-items-center">
-                    <span>Adicionar ao carrinho</span> 
-                    <i class="material-symbols-outlined" style="margin-left:5px;"> add_circle </i>
-                </a>
-            </div>
-        </div>`;
-    })
-}
-
-inicializarLoja();
+var links = document.getElementsByTagName('a');
 
 atualizarCarrinho = () => {
     var containerCarrinho = document.getElementById('list-shop');
@@ -127,13 +88,72 @@ atualizarCarrinho = () => {
     })
 }
 
-var links = document.getElementsByTagName('a');
+inicializarLoja = (tipo) => {
+    Notiflix.Loading.circle();
+    setTimeout(() => {
+        var containerProdutos = document.getElementById('produtos');
+        containerProdutos.innerHTML = "";
+        items.map((val)=>{
+            if ( tipo == 0 || val.tipo == tipo){
+                containerProdutos.innerHTML += `
+        
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-12 articles">
+                    <div class="articles-content">
+                        <img src="`+ val.img +`" alt="" srcset="">
+                        <br/>
+                        <h3>`+val.nome+`</h3>
+                        <p>`+val.descricao+`</p>
+        
+                        <div class="div-size mb-3">
+                            <button class="size">PP</button>
+                            <button class="size">P</button>
+                            <button class="size">M</button>
+                            <button class="size">G</button>
+                            <button class="size">GG</button>
+                        </div>
+        
+                        <select class="form-select mb-3" aria-label="Default select example">
+                            <option selected>Selecione</option>
+                            <option value="1">Preto</option>
+                            <option value="2">Azul</option>
+                            <option value="3">Vermelho</option>
+                            <option value="4">Rosa</option>
+                        </select>
+        
+                        <p class="w-100 text-center p-2 rounded" 
+                        style="font-size:18px; background-color: #eeeeee; font-weight: 600;"> 
+                            R$ `+val.valor+`
+                        </p>
+        
+                        <a key="`+val.id+`" class="btn btn-outline-success d-flex justify-content-center align-items-center">
+                            <span>Adicionar ao carrinho</span> 
+                            <i class="material-symbols-outlined" style="margin-left:5px;"> add_circle </i>
+                        </a>
+                    </div>
+                </div>`;
+            }
+        })
 
-for(var i = 0; i < links.length; i++){
-    links[i].addEventListener("click", function(){
-        let key = this.getAttribute('key');
-        items[key].quantidade++;
-        atualizarCarrinho();
-        return false;
-    })
+        for(var i = 0; i < links.length; i++){
+            links[i].addEventListener("click", function(){
+                let key = this.getAttribute('key');
+                items[key].quantidade++;
+                atualizarCarrinho();
+                Notiflix.Report.success(
+                    'Produto adicionado com sucesso!',
+                    '',
+                    'Ok',
+                );    
+                document.getElementById("NXReportButton").classList.add("text-center");
+                document.getElementById("NXReportButton").classList.add("text-decoration-none");
+                document.getElementById("NXReportButton").classList.add("w-100");
+                document.getElementsByClassName("notiflix-report-message").classList.add("d-none");
+                return false;
+            })
+        }
+        
+        Notiflix.Loading.remove();
+    }, 500);
 }
+
+inicializarLoja(0);
